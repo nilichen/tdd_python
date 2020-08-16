a4159a20a57133bdee473179c6c035d8082814a5
nilichen <lichenni1990@gmail.com>
2020-08-16 01:32:02 -0700

    fix fabric bug

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -35,8 +35,8 @@ def _update_settings(source_folder, site_name):
     sed(settings_path, "DEBUG = True", "DEBUG = False")
     sed(
         settings_path,
-        'ALLOWER_HOSTS = .+$',
-        f'ALLOWED_HOSTS = ["{site_name}"]"'
+        'ALLOWED_HOSTS = .+$',
+        f'ALLOWED_HOSTS = ["{site_name}"]'
     )
     secret_key_file = source_folder + '/superlists/secrect_key.py'
     if not exists(secret_key_file):
