9188f587920423dee8169a62615cedcc0f388e15
nilichen <lichenni1990@gmail.com>
2020-08-15 21:29:03 -0700

    has-error disappeared when start typing

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -5,7 +5,7 @@ from unittest import skip
 
 from lists.views import home_page
 from lists.models import Item, List
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import ItemForm, ExistingListItemForm, EMPTY_ITEM_ERROR
 
 
 class HomePageTest(TestCase):
@@ -108,7 +108,7 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get(f'/lists/{list_.id}/')
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text')
 
     def post_invalid_input(self):
@@ -129,13 +129,12 @@ class ListViewTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_list_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(text='textey', list=list1)
